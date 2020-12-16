93045e3457427706845baeeb7b673796044ed47c
amandagalemmo <amandagalemmo@gmail.com>
2020-12-15 23:41:30 -0800

    canvas now resizes to window
    testing out added coloring functionality

--- minesweeperscript.js
+++ minesweeperscript.js
@@ -1,25 +1,33 @@
+/*@TODO:
+    - create a newGame method 
+    - allow map to resize with window? this is a stretch goal*/
+
 // Initializations______________________________________________________________
 var canvas = document.getElementById('gameBoard');
 var ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 
-const TILEDIM = 24;
-const ROWS = 9;
-const COLS = 9;
-const numMines = 10;
+const TILEDIM = 24;                                   // Dimensions of each tile
+//const ROWS = Math.floor(ctx.canvas.height / TILEDIM); // might not always be a constant
+//const COLS = Math.floor(ctx.canvas.width / TILEDIM); // might not always be a constant
+
+const ROWS = 5;
+const COLS = 5;
+
 const numTiles = ROWS * COLS;
-var playerInit = getRandomInt(numTiles);
-var playerR = Math.floor(playerInit / ROWS);
-var playerC = playerInit - playerR * ROWS;
+const numMines = Math.ceil(numTiles * .125);
 
-var bLeft = new Array();
-var bRight = new Array();
-for (let i = 0; i < numTiles; i+=COLS) {bLeft.push(i);} // ew ish hacky fix
-for (let i = COLS-1; i < numTiles; i+=COLS) {bRight.push(i);}
+var leftPadding = Math.ceil((ctx.canvas.width - (COLS * TILEDIM)) / 2);
+var topPadding = Math.ceil((ctx.canvas.height - (ROWS * TILEDIM)) / 2);
 
-var boardInit = initializeBoard();
+var playerInit = getRandomInt(numTiles);              // player initial location
+var playerR = Math.floor(playerInit / COLS);          // player initial R coord
+var playerC = playerInit - playerR * COLS;            // player initial C coord
 
-var mineLocations = boardInit.mineLoc,
-    hints = boardInit.hints;
+var adjacent = initializeBoard();
+
+var score = 0;
 
 // Palette
 var colors = {
@@ -28,36 +36,37 @@ var colors = {
   seenDistant: '#b56578',
   borderNear: '#e56b6f',
   seenNear: '#eaac8b',
-  player: '#a3d6c8' //403130?
+  player: '#a3d6c8'
 }
 
 var tiles = [];
-var tileNum = 0;
 for (var r = 0; r < ROWS; r++) {
   tiles[r] = [];
   for (var c = 0; c < COLS; c++) {
     tiles[r][c] = {
-      x : 0,
-      y: 0,
-      num: tileNum,
+      cX : c * TILEDIM,      //coords for canvas
+      cY: r * TILEDIM,       //coords for canvas
+      num: (r * COLS + c),
       visited: false,
-      mine: mineLocations[tileNum],
-      hint: hints[tileNum],
-      color = colors.hidden
+      neighbors: adjacent[(r * COLS + c)],
+      color: colors.hidden
     };
-    if (tileNum === playerInit) {
-      tiles[r][c].visited = true;  // change to viewable?
-    }
-    tileNum++;
   }
 }
 
 // UI___________________________________________________________________________
-
+window.addEventListener('resize', reportWindowSize);
 document.addEventListener('keydown', keyDownHandler, {once: true});
 
+function reportWindowSize() {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    leftPadding = Math.ceil((ctx.canvas.width - (COLS * TILEDIM)) / 2);
+    topPadding = Math.ceil((ctx.canvas.height - (ROWS * TILEDIM)) / 2);
+    draw();
+}
+
 function keyDownHandler(e) {
-  console.log('keydown');
   if (e.key === "Right" || e.key === "ArrowRight" || e.key === 'A') {
     if (playerC < COLS - 1) playerC++;
   } else if (e.key === "Left" || e.key === "ArrowLeft" || e.key === 'D') {
@@ -66,12 +75,12 @@ function keyDownHandler(e) {
     if (playerR < ROWS  - 1) playerR++;
   } else if (e.key === "Up" || e.key === "ArrowUp" || e.key === 'W') {
     if (playerR > 0) playerR--;
-  } //else if (e.key === "Esc" || e.key === "Escape") {
-  // } else if (e.key === "Spacebar" || e.key === " ") {
-  // }
+  }
 
-  setTimeout(function() {document.addEventListener('keydown', 
-                                    keyDownHandler, {once: true})}, 250);
+  setTimeout(function() {
+    move();
+    draw();
+    document.addEventListener('keydown', keyDownHandler, {once: true});}, 250);
 }
 
 // Helper functions_____________________________________________________________
@@ -79,20 +88,25 @@ function getRandomInt(numTiles) {
   return Math.floor(Math.random() * numTiles);
 }
 
+// @TODO: must refactor this
 function initializeBoard() {
-  let mineLoc = new Array(numTiles).fill(false);
-  let hints = new Array(numTiles).fill(0);
+  let adjacent = new Array(numTiles).fill(0);
   let m = 0;
+  // The following is a hacky way to establish borders when counting adjacency
+  var bLeft = new Array();
+  var bRight = new Array();
+  for (let i = 0; i < numTiles; i+=COLS) {bLeft.push(i);}
+  for (let i = COLS-1; i < numTiles; i+=COLS) {bRight.push(i);}
 
   while (m < numMines) {
     let i = getRandomInt(numTiles);
     // @TODO: do not like that it's a while loop
     while (true) {
-      if (i === playerInit || mineLoc[i]) {
+      if (i === playerInit || adjacent[i] === 9) {
         i = getRandomInt(numTiles);
       } else {break;}
     }
-    mineLoc[i] = true;
+    adjacent[i] = 9;
     for (let y = -COLS; y <= COLS; y += COLS) {
       if (i + y < 0 || i + y >= numTiles) {continue;}
       for (let x = -1; x < 2; x++) {
@@ -100,16 +114,14 @@ function initializeBoard() {
         else if (bLeft.includes(i) && x === -1) {continue;}
         else if (bRight.includes(i) && x === 1) {continue;}
         j = i + x + y;
-        if (mineLoc[j]) {
-          hints[j] = '*';
-        } else {
-          hints[j]++;
+        if (adjacent[j] < 9) {
+          adjacent[j]++;
         }
       }
     }
     m++;
   }
-  return {mineLoc, hints};
+  return adjacent;
 }
 
 function prettyPrint() {
@@ -125,44 +137,133 @@ function prettyPrint() {
 
 function drawBoard() {
   for (var r = 0; r < ROWS; r++) {
-    for (var c = 0; c < COLS; c++) {
-      if (tiles[r][c].mine) {
-        color = colors['seenNear'];
-      } else if (!tiles[r][c].visited) {
-        color = colors['hidden'];
-      }
-      
-      tiles[r][c].x = (c * (TILEDIM));
-      tiles[r][c].y = (r * (TILEDIM));
-      
-      ctx.beginPath();
-      ctx.rect(tiles[r][c].x, tiles[r][c].y, TILEDIM, TILEDIM);
-      ctx.fillStyle = color;
-      ctx.fill();
-      ctx.closePath();
+    for (var c = 0; c < COLS; c++) {     
+      drawTile(r,c);
+      drawHint(r,c);
+      //if (tiles[r][c].visited) {drawHint(r,c)};
     }
   }
 }
 
+function drawTile(r, c) {
+  ctx.beginPath();
+  ctx.rect(tiles[r][c].cX + leftPadding, tiles[r][c].cY + topPadding, 
+    TILEDIM, TILEDIM);
+  ctx.fillStyle = tiles[r][c].color;
+  ctx.fill();
+  ctx.closePath();
+}
+
 function drawPlayer() {
   ctx.beginPath();
-  ctx.rect(tiles[playerR][playerC].x, tiles[playerR][playerC].y, 
+  ctx.rect(tiles[playerR][playerC].cX + leftPadding, tiles[playerR][playerC].cY + topPadding, 
            TILEDIM, TILEDIM);
   ctx.fillStyle = colors['player'];
   ctx.fill();
   ctx.closePath();
 }
 
-function drawHints() {
+function drawHint(r, c) {
   ctx.font = '12px Arial';
   ctx.fillStyle = 'white';
-  for (var r = 0; r < ROWS; r++) {
-    for (var c = 0; c < COLS; c++) {
-      ctx.fillText(`${tiles[r][c].hint}`, 
-                    tiles[r][c].x + 8, tiles[r][c].y + 16, TILEDIM);
-      /*ctx.fillText(`${tiles[r][c].num}`, 
-      tiles[r][c].x + 8, tiles[r][c].y + 16, TILEDIM);*/
+  ctx.fillText(tiles[r][c].neighbors>0 ? `${tiles[r][c].neighbors}` : ' ', 
+    tiles[r][c].cX + 8 + leftPadding, tiles[r][c].cY + 16+ topPadding, TILEDIM);
+  /*ctx.fillText(`${tiles[r][c].num}`, 
+      tiles[r][c].cX + 8, tiles[r][c].cY + 16, TILEDIM);*/
+}
+
+function move() {
+  if (tiles[playerR][playerC].neighbors === 9) {    // if tile is mine
+    gameOver();
+  } 
+  // if the tile we just moved to hasn't been visited...
+  if (!tiles[playerR][playerC].visited) {
+    tiles[playerR][playerC].visited = true;
+    //console.log(`[[${playerR},${playerC}]]`);
+    
+    let exposeQ = [];
+    let currQueue = new Set();
+    exposeQ.push(tiles[playerR][playerC]);
+    while (exposeQ.length > 0 && exposeQ[0] !== null) {
+      console.log('exposeQ ' + exposeQ + ' len: ' + exposeQ.length);
+      let currNode = exposeQ.shift();
+      console.log('currNode: ' + currNode.cY + ' ' + currNode.cX);
+      //handles current tile, returns list of exposure-ready neighbors
+      let result = handleTile(currNode.cY / TILEDIM, currNode.cX / TILEDIM);
+      if (result !== null) {
+        console.log('result: ' + result + ' result length: ' + result.length);
+        console.log(result[0] === result[1]);
+        for (let r of result) {
+          console.log(`\t[${r.cY/TILEDIM}][${r.cX/TILEDIM}]`);
+          currQueue.add(r);
+        }
+        console.log(currQueue.size);
+        exposeQ = [...currQueue];
+      }
     }
+  }
+}
+
+function handleTile(r, c) {
+  try {
+    assignColor(tiles[r][c]);
+  } catch (error) {
+    console.error(error);
+    console.log(r + ' ' + c);
+    return;
+  }
+  let blocked = 0;
+  let toQueue = [];
+  let zero = false;
+  tiles[r][c].visited = true;
+  for (i = -1; i <= 1; i += 2) {
+    console.log(i);
+    if (r + i > 0 && r + i < ROWS-1) {
+      console.log(`\t[${r + i}][${c}]`);
+      if (!tiles[r + i][c].visited) {
+        if (tiles[r + i][c].neighbors > 0) {
+          blocked++;
+          if (tiles[r + i][c].neighbors < 9) {
+            toQueue.push(tiles[r + i][c]);
+          }
+        } else {
+          zero = true;
+          toQueue.push(tiles[r + i][c]);
+        }
+      }
+    } 
+    if (c + i > 0 && c + i < COLS-1) {
+      if (!tiles[r][c + i].visited) {
+        if (tiles[r][c + i].neighbors > 0) {
+          blocked++;
+          if (tiles[r][c + i].neighbors < 9) {
+            toQueue.push(tiles[r][c + i]);
+          }
+        } else {
+          zero = true;
+          toQueue.push(tiles[r][c + i])
+        }
+      }
+    }
+  }
+  if (blocked === 4 || !zero) {
+    console.log('blocked');
+    return null;
+  }
+  return toQueue;
+}
+
+function gameOver() {
+  console.log('GAME OVER!')
+}
+
+function assignColor(tile) {
+  if (tile.neighbors === 0) {
+    tile.color = colors.seenNear;
+  } else if (tile.neighbors === 9) {
+    return;
+  } else {
+    tile.color = colors.borderNear;
   }
 }
 
@@ -170,16 +271,7 @@ function drawHints() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBoard();
-  
-  drawHints();
   drawPlayer();
-
-  // once event is triggered, run draw and delay event listener .5sec, 
-  // but continue to draw. maybe idle enemy animations. dont have to worry 
-  // at the minute though.
-  
-  //setTimeout(function() {requestAnimationFrame(draw)}, 500);
-  requestAnimationFrame(draw);
 }
-//trigger draw on successful keydown
+move();
 draw();
